0e3768697a6cf80708cd1792e30befa9e6118f15
Vladyslav_Krysanov <Vladyslav_Krysanov@epam.com>
2019-12-11 19:48:47 +0200

    PAT-14 change mongodb credentials

--- server/index.js
+++ server/index.js
@@ -13,7 +13,7 @@ const app = express();
 // Passport Config
 require('./config/passport')(passport);
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 
 // Connect to MongoDB
 mongoose
